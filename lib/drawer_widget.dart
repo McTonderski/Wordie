491de68290813268860651f6047359d5af7307cb
McTonderski <maciejtonderski.mt@gmail.com>
2020-03-26 01:13:57 +0100

    Started with drawer and navbar not yet finished

--- lib/drawer_widget.dart
+++ lib/drawer_widget.dart
@@ -9,7 +9,51 @@ class DrawerWidget extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: <Widget
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.white,
+                  Colors.white70
+                ]
+              )
+            ), 
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomListTile(
+                    name: Text("Strona Główna", style: TextStyle(fontSize: 16.0),),
+                    icon: Icon(Icons.home)
+                  )
+                ],)
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final Text name;
+  final Icon icon;
+  const CustomListTile({Key key, @required this.name, @required this.icon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          icon,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: name,
+          )
+        ]
       ),
     );
   }
--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class NavBarWordie extends StatelessWidget {
-  const NavBarWordie({Key key}) : super(key: key);
+class NavBarWordie extends StatelessWidget implements PreferredSizeWidget{
+  final Color backgroundColor = Colors.red;
+  final Text title;
+  final AppBar appBar;
+  final List<Widget> widgets;
+  const NavBarWordie({Key key, this.title, this.appBar, this.widgets})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      
-    )
+      title: Text("wordie"),
+    );
   }
+
+  @override
+  Size get preferredSize => new Size.fromHeight(appBar.preferredSize.height);
 }
