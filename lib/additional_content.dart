4c01a4ae1575367c0f2f3c716984c45739512b14
Bartosz <bart.augus@onet.pl>
2020-03-27 01:07:36 +0100

    fixed scrolling

--- lib/additional_content.dart
+++ lib/additional_content.dart
@@ -2,85 +2,89 @@ import 'package:flutter/material.dart';
 import 'package:wordie/all_addon.dart';
 import 'package:wordie/nav_bar.dart';
 
-class AddonPage extends StatefulWidget{
+class AddonPage extends StatefulWidget {
   AddonPage();
-  @override
-  _AddonPageState createState() =>new  _AddonPageState();
 
+  @override
+  _AddonPageState createState() => new _AddonPageState();
 }
 
-class _AddonPageState extends State<AddonPage>{
-Widget addon(textIn) {
-  
+class _AddonPageState extends State<AddonPage> {
+  Widget addon(textIn) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal:100,vertical: 30),
-    child: ClipOval(
-                  
-                  child:Container(
-                    width: 220,
-                    height: 220,
-                    color: Color.fromARGB(110, 85, 131, 84),
-                    //decoration
-                    child: FlatButton(
-                      child: Text(
-                        textIn,
-                        style: TextStyle(fontSize: 35,fontFamily: 'SourceSerifPro',),),
-                      onPressed: (){Navigator.push(context,MaterialPageRoute(builder:(context)=>AllAddonPage()));},
-                    ),)
-                ,));
+        padding: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
+        child: ClipOval(
+          child: Container(
+            width: 220,
+            height: 220,
+            color: Color.fromARGB(110, 85, 131, 84),
+            //decoation
+            child: FlatButton(
+              child: Text(
+                textIn,
+                style: TextStyle(
+                  fontSize: 35,
+                  fontFamily: 'SourceSerifPro',
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AllAddonPage()));
+              },
+            ),
+          ),
+        ));
   }
 
-  Widget build(BuildContext context){
-    return Scaffold
-    (appBar: AppBar(),
-    body: Center(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Text(
-            "Dodatkowe materiały",
-            style: TextStyle(
-              fontSize: 48,
-              fontFamily: 'SourceSerifPro',  
-            ),),
-          Center(child: LayoutBuilder(builder: (context, constraints) {
-              if (constraints.maxWidth > 600) {
-                return SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                
-                addon('Wszystkie'),
-                addon('Darmowe')
-              ]));
-              } else {
-                return SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                
-                addon('Wszystkie'),
-                addon('Darmowe')
-              ]));
-              }
-            })
-            // child: Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: <Widget>[
-                
-            //     addon('Wszystkie'),
-            //     addon('Darmowe')
-            //   ],
-            // ),
-          )
-        ],
-      ),
-      ),
-      ));
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  "Dodatkowe materiały",
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontFamily: 'SourceSerifPro',
+                  ),
+                ),
+                Center(child: LayoutBuilder(builder: (context, constraints) {
+                  if (constraints.maxWidth > 600) {
+                    return SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              addon('Wszystkie'),
+                              addon('Darmowe')
+                            ]));
+                  } else {
+                    return SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              addon('Wszystkie'),
+                              addon('Darmowe')
+                            ]));
+                  }
+                })
+                    // child: Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //   children: <Widget>[
+
+                    //     addon('Wszystkie'),
+                    //     addon('Darmowe')
+                    //   ],
+                    // ),
+                    )
+              ],
+            ),
+          ),
+        ));
   }
 }
