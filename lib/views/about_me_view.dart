eb06fce39ca55862f40f6d44ffe20d615831357c
A-NGJ <anagaj99@gmail.com>
2020-04-02 10:03:17 +0200

    added flexible dimensions

--- lib/views/about_me_view.dart
+++ lib/views/about_me_view.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:wordie/widgets/nav_bar.dart';
@@ -20,35 +21,61 @@ class AboutMeView extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+
+    double height_ratio = MediaQuery.of(context).size.height/WINDOW_HEIGHT;
+    double width_ratio = MediaQuery.of(context).size.width/WINDOW_WIDTH;
+
     return Scaffold(
       backgroundColor: primaryBackgroundColor,
       appBar: NavBarWordie(title: Text('Wordie'), widgets: <Widget>[],),
       body: ListView(
           children: <Widget>[
-            SizedBox(height: 50.0,),
+            SizedBox(height: 50.0*height_ratio,),
             Padding(
-              padding: EdgeInsets.fromLTRB(104, 0, 105, 0),
+              padding: EdgeInsets.fromLTRB(104*width_ratio, 0, 105*width_ratio, 0),
               child: Container(
-                width: 943,
-                height: 548,
+                width: 943*width_ratio,
+                height: 548*height_ratio,
                 child: Stack( 
                   children: <Widget>[
                     Positioned(
+                      bottom: 0,
+                      left: 368*width_ratio,
+                      right: 91*height_ratio,
+                      top: 73*width_ratio,
+                      child: Container(
+                        width: 484*width_ratio,
+                        height: 475*height_ratio,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFFB6CAB5)
+                          )
+                        ),
+                    ),
+                    Positioned(
+                      top: 52*height_ratio,
+                      right: 175*width_ratio,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/11.png'),
+                        radius: 193*sqrt(width_ratio*height_ratio),
+                      ),
+                    ),
+                    Positioned(
                       top: 0,
-                      left: 200,
+                      left: 200*width_ratio,
                       child: Text(
                         'Poznaj mnie',
                         style: TextStyle(
-                          fontSize: 48,
+                          fontSize: 48*sqrt(height_ratio),
                         ),
                       ),
                     ),
                     Positioned(
-                      left: 200,
+                      left: 200*width_ratio,
                       bottom: 0,
                       child: Container(
-                        width: 481,
-                        height: 465,
+                        width: 481*width_ratio,
+                        height: 465*width_ratio,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -61,28 +88,6 @@ class AboutMeView extends StatelessWidget{
                             )
                           )).toList()
                         ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 368,
-                      right: 91,
-                      top: 73,
-                      child: Container(
-                        width: 484,
-                        height: 475,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFFB6CAB5)
-                          )
-                        ),
-                    ),
-                    Positioned(
-                      top: 52,
-                      right: 175,
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage('assets/images/11.png'),
-                        radius: 193,
                       ),
                     ),
                   ],
