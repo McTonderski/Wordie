53d8a03acb6948d3ae886d8bd08a1fc1eb794c0d
McTonderski <maciejtonderski.mt@gmail.com>
2020-04-03 20:30:43 +0200

    fixed navigation

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -22,7 +22,7 @@ class DrawerItemList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String currentRoute = AboutMeView.routeName;
+    String currentRoute = getCurrentRouteName(context);
 
     
     double window_width = MediaQuery.of(context).size.width;
