d304de19450255413f28d9b91d8d3e1f2b02abfd
Krzysztof Czech <kczech123kczech123@gmail.com>
2020-04-03 19:36:56 +0200

    bug fixed  Please enter the commit message for your changes. Lines starting

--- lib/views/Contact_Page.dart
+++ lib/views/Contact_Page.dart
@@ -17,121 +17,121 @@ class _ContactState extends State<ContactPage>{
     return Scaffold(
       drawerScrimColor: Colors.transparent,
       appBar: NavBarWordie(title: Text('Wordie'), widgets: <Widget>[],),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-          child: Center(
+      body:Center(
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Flexible(
-                  fit: FlexFit.loose,
-                  flex: 2,
-                  child :Text(
-                    "Kontakt",
-                    style: TextStyle(
-                      fontSize: 40,
-                      //fontFamily:  
-                    ),
-                    textAlign: TextAlign.center,
-                    ),
-                ),
-                
-                Flexible(
-                  fit: FlexFit.loose,
-                  flex:1,
-                  child:Text(
-                    "email@com",
-                    style: TextStyle(
-                    fontSize: 20,
-                    //fontFamily:  
-                    ),
-                  ), 
-                ),
-
-                Flexible(
-                  fit :FlexFit.loose,
-                  flex:6,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.all(10),
-                            width: 300,
-                            child: TextField(
-                                decoration: InputDecoration(
-                                fillColor: Color.fromRGBO(225, 204, 130, 0.22),
-                                filled: true,
-                                hintText: 'Imie i nazwisko',
-                                ),
-                                  autofocus: false,
-                                )
-                              ),
-                          Container(
-                            width: 300,
-                            child: TextField(
-                                decoration: InputDecoration(
-                                fillColor: Color.fromRGBO(225, 204, 130, 0.22),
-                                filled: true,
-                                hintText: 'Email',
-                                ),
-                                  autofocus: false,
-                                )
-                              ),
-                          Container(
-                            margin: EdgeInsets.all(10),
-                            width: 300,
-                            child: TextField(
-                                decoration: InputDecoration(
-                                fillColor: Color.fromRGBO(225, 204, 130, 0.22),
-                                filled: true,
-                                hintText: 'Telefon',
-                                ),
-                                  autofocus: false,
-                                )
-                              )
-                        ]
-                      ) ,
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                        Container(
-                              margin: EdgeInsets.all(10),
-                              width: 400,
-                              child: TextField(
-                                  keyboardType: TextInputType.multiline,
-                                  maxLines: 11,
-                                  selectionHeightStyle: BoxHeightStyle.max ,
-                                  decoration: InputDecoration(
-                                  fillColor: Color.fromRGBO(225, 204, 130, 0.22),
-                                  filled: true,
-                                  hintText: 'Wiadomość',
-                                  ),
-                                    autofocus: false,
-                                  
-                                ),
-                            ),
-                        ]
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(40),
+                    child :Text(
+                      "Kontakt",
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontFamily:  'SourceSerifPro',
                       ),
-                    ]
+                      textAlign: TextAlign.center,
+                      ),
                   ),
-                ),
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: IconButton(
-                    icon: Icon(Icons.mail),                  
-                    onPressed: () {},                  
-                    )
-                )   
-              ],
+                  
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child:Text(
+                      "angelika@wordie.no",
+                      style: TextStyle(
+                      fontSize: 20,
+                      fontFamily:  'SourceSerifPro',
+                      ),
+                    ), 
+                  ),
+
+                  Container(
+                    child: Padding(
+                      padding: EdgeInsets.only(top:10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Container(
+                                    margin: EdgeInsets.all(40),
+                                    width: 300,
+                                    child: TextField(
+                                        decoration: InputDecoration(
+                                        fillColor: Color.fromRGBO(225, 204, 130, 0.22),
+                                        filled: true,
+                                        hintText: 'Imie i nazwisko',
+                                        ),
+                                          autofocus: false,
+                                        )
+                                      ),
+                                 
+                                  Container(
+                                    width: 300,
+                                    child: TextField(
+                                        decoration: InputDecoration(
+                                        fillColor: Color.fromRGBO(225, 204, 130, 0.22),
+                                        filled: true,
+                                        hintText: 'Email',
+                                        ),
+                                          autofocus: false,
+                                        )
+                                      ),
+                                 
+                                  Container(
+                                    margin: EdgeInsets.all(40),
+                                    width: 300,
+                                    child: TextField(
+                                        decoration: InputDecoration(
+                                        fillColor: Color.fromRGBO(225, 204, 130, 0.22),
+                                        filled: true,
+                                        hintText: 'Telefon',
+                                        ),
+                                          autofocus: false,
+                                        )
+                                      )
+                                ]
+                              ) ,
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                Container(
+                                      margin: EdgeInsets.all(10),
+                                      width: 400,
+                                      child: TextField(
+                                          keyboardType: TextInputType.multiline,
+                                          maxLines: 11,
+                                          //textAlign: TextAlign.center,
+                                          selectionHeightStyle: BoxHeightStyle.max ,
+                                          decoration: InputDecoration(
+                                          fillColor: Color.fromRGBO(225, 204, 130, 0.22),
+                                          filled: true,
+                                          hintText: 'Wiadomość',
+                                          ),
+                                            autofocus: false,
+                                          
+                                        ),
+                                    ),
+                                ]
+                              ),
+                            ]
+                        ),
+                          ),
+                    ),
+                    Container(
+                    child: IconButton(
+                      icon: Icon(Icons.mail),                  
+                      onPressed: () {},                  
+                      )
+                  )   
+                ]),
+              ),
             ),
           ),
-      ))
     );
   }
 }
