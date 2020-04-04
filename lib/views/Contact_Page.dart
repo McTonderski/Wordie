f79039b773cc8445e3bff77645866bd4a2336bae
Krzysztof Czech <kczech123kczech123@gmail.com>
2020-04-04 17:44:32 +0200

    bug fixed

--- lib/views/Contact_Page.dart
+++ lib/views/Contact_Page.dart
@@ -13,6 +13,45 @@ class ContactPage extends StatefulWidget {
   _ContactState createState() => new _ContactState();
 }
 
+ Widget multilineText(hinttext) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      width: 400,
+      child: TextField(
+        keyboardType: TextInputType.multiline,
+        maxLines: 11,
+        //textAlign: TextAlign.center,
+        selectionHeightStyle: BoxHeightStyle.max,
+        decoration: InputDecoration(
+          fillColor:
+              Color.fromRGBO(225, 204, 130, 0.22),
+          filled: true,
+          hintText: hinttext,
+        ),
+        autofocus: false,
+      ),
+    );
+ }
+
+ Widget textFieldUsed(hinttext) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      width: 300,
+      child: TextField(
+        decoration: InputDecoration(
+          fillColor: Color.fromRGBO(
+              225, 204, 130, 0.22),
+          filled: true,
+          hintText: hinttext,
+        ),
+        autofocus: false,
+      ));
+ }
+
+
+
+
+
 class _ContactState extends State<ContactPage> {
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,109 +61,76 @@ class _ContactState extends State<ContactPage> {
       backgroundColor: primaryBackgroundColor,
       body: Center(
         child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(40),
-                    child: Text(
-                      "Kontakt",
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontFamily: 'SourceSerifPro',
-                      ),
-                      textAlign: TextAlign.center,
+        scrollDirection: Axis.vertical,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Text(
+                    "Kontakt",
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontFamily: 'SourceSerifPro',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "angelika@wordie.no",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'SourceSerifPro',
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "angelika@wordie.no",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'SourceSerifPro',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: LayoutBuilder(builder: (context, constraints) {
+                           if (constraints.maxWidth >700) {
+                              return  SingleChildScrollView(
+                                scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 children: <Widget>[
-                                  Container(
-                                      margin: EdgeInsets.all(40),
-                                      width: 300,
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          fillColor: Color.fromRGBO(
-                                              225, 204, 130, 0.22),
-                                          filled: true,
-                                          hintText: 'Imie i nazwisko',
-                                        ),
-                                        autofocus: false,
-                                      )),
-                                  Container(
-                                      width: 300,
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          fillColor: Color.fromRGBO(
-                                              225, 204, 130, 0.22),
-                                          filled: true,
-                                          hintText: 'Email',
-                                        ),
-                                        autofocus: false,
-                                      )),
-                                  Container(
-                                      margin: EdgeInsets.all(40),
-                                      width: 300,
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          fillColor: Color.fromRGBO(
-                                              225, 204, 130, 0.22),
-                                          filled: true,
-                                          hintText: 'Telefon',
-                                        ),
-                                        autofocus: false,
-                                      ))
-                                ]),
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Container(
-                                    margin: EdgeInsets.all(10),
-                                    width: 400,
-                                    child: TextField(
-                                      keyboardType: TextInputType.multiline,
-                                      maxLines: 11,
-                                      //textAlign: TextAlign.center,
-                                      selectionHeightStyle: BoxHeightStyle.max,
-                                      decoration: InputDecoration(
-                                        fillColor:
-                                            Color.fromRGBO(225, 204, 130, 0.22),
-                                        filled: true,
-                                        hintText: 'Wiadomość',
-                                      ),
-                                      autofocus: false,
-                                    ),
-                                  ),
-                                ]),
-                          ]),
+                                  Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    textFieldUsed("Imie i Nazwisko"),
+                                    textFieldUsed("Email"),
+                                    textFieldUsed("Telefon")
+                                  ]),
+                                  Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                  multilineText("Wiadomość")  
+                                  ]),
+                                ]));
+                            } else {
+                              return  Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                textFieldUsed("Imie i Nazwisko"),
+                                textFieldUsed("Emaillll"),
+                                textFieldUsed("Telefon"),
+                                multilineText("Wiadomość"),
+                                Container(
+                                    child: IconButton(
+                                  icon: Icon(Icons.mail),
+                                  onPressed: () {},
+                                ))  
+                              ]
+                                );
+                            }
+                    }                       
                     ),
                   ),
-                  Container(
-                      child: IconButton(
-                    icon: Icon(Icons.mail),
-                    onPressed: () {},
-                  ))
-                ]),
-          ),
+                ),
+                
+              ]),
         ),
       ),
     );
